889f5477990e16d99200a7408e5ce4bc288d6de7
Carlos Martín Nieto <cmn@dwim.me>
2022-09-01 08:38:08 +0200

    Add PI messages to the runner stuff

--- src/runner.rs
+++ src/runner.rs
@@ -29,3 +29,23 @@ pub struct Registration {
     /// UUID as passed in to the plugin during startup
     pub uuid: String,
 }
+
+/// A message sent to the property inspector
+///
+/// Contrary to the "regular" [`crate::property_inspector::Message`] we can't
+/// specify the types for the settings or message from the property inspector as
+/// those are different on each plugin. The best we can do is leave them as JSON
+/// values.
+///
+/// [Official Documentation](https://developer.elgato.com/documentation/stream-deck/sdk/events-received/)
+pub type PropertyInspectorMessage = crate::property_inspector::Message<Value, Value, Value>;
+
+/// A message received from the property inspector
+///
+/// Contrary to the "regular" [`crate::property_inspector::Message`] we can't
+/// specify the types for the settings or message from the property inspector as
+/// those are different on each plugin. The best we can do is leave them as JSON
+/// values.
+///
+/// [Official Documentation](https://developer.elgato.com/documentation/stream-deck/sdk/events-sent/)
+pub type PropertyInspectorMessageIn = crate::property_inspector::MessageOut<Value, Value, Value>;
